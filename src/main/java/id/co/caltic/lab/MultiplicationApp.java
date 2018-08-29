a0b207d9040410d6b5319d1c88aaae85a883c2a4
Heru Pranoto <herupranoto.ugm@gmail.com>
2018-08-29 23:51:07 +0700

    Adding implementation with their test case

--- src/main/java/id/co/caltic/lab/MultiplicationApp.java
+++ src/main/java/id/co/caltic/lab/MultiplicationApp.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MultipicationApp {
+public class MultiplicationApp {
 
     public static void main( String[] args ) {
-        SpringApplication.run(MultipicationApp.class, args);
+        SpringApplication.run(MultiplicationApp.class, args);
     }
 }
--- src/test/java/id/co/caltic/lab/MultiplicationAppTest.java
+++ src/test/java/id/co/caltic/lab/MultiplicationAppTest.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class MultipicationAppTest {
+public class MultiplicationAppTest {
     
     @Test
     public void contextLoads() {
